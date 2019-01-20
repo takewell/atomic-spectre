6fde970c2ae82e28a9b2688a37df90cfddfc5faf
yuya takei <takewell.dev@gmail.com>
2019-01-20 22:04:19 +0900

    Edit prime-color

--- gulpfile.js
+++ gulpfile.js
@@ -11,65 +11,98 @@ var paths = {
   doc: './docs/src/scss/*.scss'
 };
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('./**/*.scss', ['build']);
   gulp.watch('./**/*.scss', ['docs']);
   gulp.watch('./**/*.pug', ['docs']);
 });
 
-gulp.task('build', function () {
-  gulp.src(paths.source)
-    .pipe(sass({
+gulp.task('build', function() {
+  gulp
+    .src(paths.source)
+    .pipe(
+      sass({
         outputStyle: 'compact',
         precision: 10
-      })
-      .on('error', sass.logError)
+      }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./dist'))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build -t', function() {
+  gulp
+    .src(paths.source)
+    .pipe(
+      sass({
+        outputStyle: 'compact',
+        precision: 10
+      }).on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('../public'));
 });
 
-gulp.task('docs', function () {
-  gulp.src(paths.doc)
-    .pipe(sass({
+gulp.task('docs', function() {
+  gulp
+    .src(paths.doc)
+    .pipe(
+      sass({
         outputStyle: 'compact',
         precision: 10
-      })
-      .on('error', sass.logError)
+      }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./docs/dist'))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('./docs/dist'));
-  gulp.src(paths.source)
-    .pipe(sass({
+  gulp
+    .src(paths.source)
+    .pipe(
+      sass({
         outputStyle: 'compact',
         precision: 10
-      })
-      .on('error', sass.logError)
+      }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./docs/dist'))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('./docs/dist'));
-  gulp.src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
+  gulp
+    .src('docs/src/**/!(_)*.pug')
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
     .pipe(gulp.dest('./docs/'));
 });
 
